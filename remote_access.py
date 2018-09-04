18861b049a76ecdc9b5b7c047aab6171a6a408ff
blair young <byhillsboro@gmail.com>
2018-09-04 11:37:13 +0100

    Fixed CLI + flexible playback

--- remote_access.py
+++ remote_access.py
@@ -2,7 +2,6 @@ import spotipy
 import spotipy.util as util
 import os
 from json.decoder import JSONDecodeError
-import json
 
 class Remote():
     def __init__(self, username, client_id, client_secret):
@@ -14,12 +13,13 @@ class Remote():
         self.token = None
         self.spotify = self.__access()
 
-    def playback(self, device):
-        if self.token:
+    def playback(self, device, song_uri=None):
+        if self.token and song_uri:
+            self.spotify.start_playback(device, uris=song_uri)
+        elif self.token:
             self.spotify.start_playback(device)
         else:
             print('Error- Token unavailable')
-
 
     def get_song_bpm(self, song_uri):
         track = self.spotify.audio_analysis(song_uri)
--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--track', '-t', default=None, help='Track URI')
+    parser.add_argument('--track', '-t', help='Track URI')
     parser.add_argument('--bpm', '-b', default=None, type=int, help= 'bpm of count in')
     args = parser.parse_args()
 
@@ -15,23 +15,26 @@ if __name__ == '__main__':
 
     phone = '743eb5597fe6dfb1855c40453baced0234e81074'
     laptop = 'b26204cd57cda44dc2890dce9444f7fe07c69ea8'
+    song_uris = [args.track]
     device = laptop
 
     count = CountIn()
     remote = Remote(username, client_id, client_secret)
 
-
     if not args.track and not args.bpm:
-        print('Defaulting to 100bpm')
+        print('Playing at default 100 BPM at last paused position ')
         args.bpm = 100
         count.count(4, args.bpm)
         remote.playback(device)
 
     elif args.track and args.bpm:
-        print('Use either track or bpm, not both')
+        bpm = remote.get_song_bpm(args.track)
+        print('Warning: Track played at {} but user has set count in BPM to {}'.format(bpm, args.bpm))
+        count.count(4, args.bpm)
+        remote.playback(device, song_uris)
 
     elif args.bpm:
-        print('Playing at custom BPM- {}'.format(args.bpm))
+        print('Playing at custom BPM- {} from last paused position'.format(args.bpm))
         count.count(4, args.bpm)
         remote.playback(device)
 
@@ -39,4 +42,4 @@ if __name__ == '__main__':
         bpm = remote.get_song_bpm(args.track)
         print('Playing at {}'.format(bpm))
         count.count(4, bpm)
-        remote.playback(device)
+        remote.playback(device, song_uris)
